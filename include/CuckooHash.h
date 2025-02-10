c6cebf4eaf6bf6cd54cafeab82e97bfb1a087149
Claudiojarala <claudio.jara.lamas22@gmail.com>
2025-02-10 00:18:00 -0500

    Create CuckooHash.h

---
+++ include/CuckooHash.h
@@ -0,0 +1,29 @@
+#ifndef CUCKOOHASH_H
+#define CUCKOOHASH_H
+
+#include <vector>
+#include "Database.h"
+
+class CuckooHash {
+private:
+    vector<Database*> table1;
+    vector<Database*> table2;
+    int capacity;
+    int totalElements;
+    const int maxLoop = 32;
+
+    int hash1(const string &key) const;
+    int hash2(const string &key) const;
+    void rehash();
+
+public:
+    CuckooHash(int cap = 11);
+    ~CuckooHash();
+    bool insert(Database* db);
+    Database* find(const string &name);
+    bool remove(const string &name);
+    void guardarEnArchivo(const string &filename) const;
+    bool cargarDesdeArchivo(const string &filename);
+};
+
+#endif // CUCKOOHASH_H
