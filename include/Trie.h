12bae9906faae10f34bd08ff6f167c34c8a11e4d
Claudiojarala <claudio.jara.lamas22@gmail.com>
2025-02-10 00:18:57 -0500

    Create Trie.h

---
+++ include/Trie.h
@@ -0,0 +1,31 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+class TrieNode {
+public:
+    unordered_map<char, TrieNode*> children;
+    bool isEnd;
+    string command;
+    TrieNode();
+};
+
+class Trie {
+private:
+    TrieNode* root;
+    void clear(TrieNode* node);
+    string findCommand(TrieNode* node);
+
+public:
+    Trie();
+    ~Trie();
+    void insert(const string &word);
+    bool search(const string &word);
+    string suggest(const string &prefix);
+};
+
+#endif // TRIE_H
