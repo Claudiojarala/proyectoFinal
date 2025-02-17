d304c31357e923f02d0339e910d6363a50e1a07c
Claudiojarala <claudio.jara.lamas22@gmail.com>
2025-02-16 22:17:53 -0500

    Update Database.h

--- include/Database.h
+++ include/Database.h
@@ -4,20 +4,22 @@
 #include <string>
 #include "CircularQueue.h"
 
-struct Database {
-    std::string name;
-    std::string dataType;
-    int capacity;
-    CircularQueue* queue;
+using namespace std;
 
-    Database(const std::string &n, const std::string &dt, int cap);
+struct Database {
+    string name;
+    string dataType;
+    int capacity;
+    CircularQueue* queue;  // Puntero a la cola circular de datos
+
+    Database(const string &n, const string &dt, int cap);
     ~Database();
-    double stringToDouble(const std::string& str) const;
-    std::string doubleToString(double value) const;
-    bool insertValue(const std::string& value);
-    std::string getValue(int index) const;
-    void serialize(std::ostream &os) const;
-    static Database* deserialize(std::istream &is);
+    double stringToDouble(const string& str) const;
+    string doubleToString(double value) const;
+    bool insertValue(const string& value);
+    string getValue(int index) const;
+    void serialize(ostream &os) const;
+    static Database* deserialize(istream &is);
 };
 
 #endif // DATABASE_H
