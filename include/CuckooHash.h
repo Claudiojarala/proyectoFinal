268acd9365e313d89153e99b76dd59f16a307b14
Claudiojarala <claudio.jara.lamas22@gmail.com>
2025-02-16 21:43:03 -0500

    Update CuckooHash.h

--- include/CuckooHash.h
+++ include/CuckooHash.h
@@ -2,31 +2,29 @@
 #define CUCKOOHASH_H
 
 #include <vector>
+#include <string>
 #include "Database.h"
-
-using namespace std;
 
 class CuckooHash {
 private:
-    vector<Database*> table1;
-    vector<Database*> table2;
+    std::vector<Database*> table1;
+    std::vector<Database*> table2;
     int capacity;
     int totalElements;
     const int maxLoop = 32;
 
-    int hash1(const string &key) const;
-    int hash2(const string &key) const;
+    int hash1(const std::string &key) const;
+    int hash2(const std::string &key) const;
     void rehash();
 
 public:
     CuckooHash(int cap = 11);
     ~CuckooHash();
-
     bool insert(Database* db);
-    Database* find(const string &name);
-    bool remove(const string &name);
-    void guardarEnArchivo(const string &filename) const;
-    bool cargarDesdeArchivo(const string &filename);
+    Database* find(const std::string &name);
+    bool remove(const std::string &name);
+    void guardarEnArchivo(const std::string &filename) const;
+    bool cargarDesdeArchivo(const std::string &filename);
 };
 
 #endif // CUCKOOHASH_H
